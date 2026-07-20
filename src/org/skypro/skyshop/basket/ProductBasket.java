301a725e7c792ed16e8d8496c6e2a51dd94fe813
User <dima.minvod@gmail.com>
2026-07-20 16:16:03 +0300

    Home work

---
+++ src/org/skypro/skyshop/basket/ProductBasket.java
@@ -0,0 +1,61 @@
+package org.skypro.skyshop.basket;
+
+import org.skypro.skyshop.product.Product;
+
+public class ProductBasket {
+    private Product[] products;
+    private int index;
+
+    public ProductBasket() {
+        this.products = new Product[5];
+        this.index = 0;
+    }
+
+    public void add(Product product) {
+        if (index >= 5) {
+            System.out.println("Невозможно добавить продукт");
+            return;
+        }
+        products[index++] = product;
+    }
+
+    public int sum() {
+        int sum = 0;
+
+        for (int i = 0; i < index; i++) {
+            sum += products[i].getPrice();
+        }
+
+        return sum;
+    }
+
+    public void print() {
+        if (index != 0) {
+            for (int i = 0; i < index; i++) {
+                System.out.println(products[i].getName() + ": " + products[i].getPrice());
+            }
+
+            System.out.println("Итого: " + sum());
+        } else {
+            System.out.println("В корзине пусто");
+        }
+    }
+
+    public boolean contains(String name) {
+        for (int i = 0; i < index; i++) {
+            if (products[i].getName().equals(name)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public void clear() {
+        for (int i = 0; i < products.length; i++) {
+            products[i] = null;
+        }
+
+        index = 0;
+    }
+}
